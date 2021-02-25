1177d3e05ec46e0587d39cda15d28b2757196192
wisma15 <79502121+wisma15@users.noreply.github.com>
2021-02-25 22:56:33 +0800

    master

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>BLOG PRIBADIKU</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div id="header">
+            <h1>BLOG PRIBADIKU</h1>
+        </div>
+    </div>
+    <div class="container">
+        <div id="navigation" class="col-sm-2">
+            <p><b><a href="under.html">HOME</a></b></p>
+        </div>
+        <div id="navigation" class="col-sm-2">
+            <p><b><a href="under.html">CONTACT</a></b></p>
+        </div>
+        <div id="navigation" class="col-sm-2">
+            <p><b><a href="under.html">ABOUT</a></b></p>
+        </div>
+    </div>
+    <div class="container">
+        <div id="main-content" class="col-sm-8">
+            <h3>Lovina</h3>
+            <img src="media/pantai.jpg" width="200px">
+            <p>Lovina atau Lovina terletak sekitar 9 Km sebelah barat kota Singaraja, ini merupakan salah satu objek wisata yang ada di Bali Utara. Wisatawan baik asing maupun lokal banyak yang berkunjung ke sana, selain untuk melihat pantainya yang masih alami, juga untuk melihat ikan lumba-lumba yang banyak terdapat di pantai ini. Dengan menyewa perahu nelayan setempat, kita dapat mendekati lumba-lumba. Berbagai penginapan mulai dari Inn hingga Cottages tersedia dengan harga yang sangat terjangkau.</p>
+        </div>
+        <div id="right-sidebar" class="col-sm-4">
+            <ul class="sidebar-list">
+                <li><h4>Artikel Terkait</h4></li>
+                <li><a href="under.html">Pantai Penimbangan</a></li>
+                <li><a href="under.html">Pantai Kuta</a></li>
+            </ul>
+
+
+        </div>
+    </div>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
